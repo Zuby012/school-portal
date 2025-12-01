99a97ac8b0d4fd49a45c8fa3a3e0af3e08490be2
Zuby012 <amahblaise0@gmail.com>
2025-12-01 16:33:21 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,14 +16,25 @@ export default defineNuxtConfig({
     },
     alias: {
       // Mock MongoDB's optional dependencies
-      '@mongodb-js/zstd': 'unenv/runtime/mock/empty',
-      'snappy': 'unenv/runtime/mock/empty',
+      '@mongodb-js/zstd': 'unenv/mock/empty',
+      'snappy': 'unenv/mock/empty',
       'kerberos': 'unenv/runtime/mock/empty',
-      'aws4': 'unenv/runtime/mock/empty',
-      'mongodb-client-encryption': 'unenv/runtime/mock/empty',
-      'mongodb-js/client-encryption': 'unenv/runtime/mock/empty',
-      'gcp-metadata': 'unenv/runtime/mock/empty',
-      'snappy/package.json': 'unenv/runtime/mock/empty',
+      'aws4': 'unenv/mock/empty',
+      'mongodb-client-encryption': 'unenv/mock/empty',
+      'mongodb-js/client-encryption': 'unenv/mock/empty',
+      'gcp-metadata': 'unenv/mock/empty',
+      'snappy/package.json': 'unenv/mock/empty',
+    },
+    rollupConfig: {
+      external: [
+        '@mongodb-js/zstd',
+        'snappy',
+        'kerberos',
+        'aws4',
+        'mongodb-client-encryption',
+        'mongodb-js/client-encryption',
+        'gcp-metadata'
+      ]
     }
   },
   app: {
